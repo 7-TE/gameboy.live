d68b0eb56cb24ca667c41bfa27f7e5c0d096daef
7TE <103470342+7-TE@users.noreply.github.com>
2022-09-19 20:42:45 -0400

    Add files via upload

---
+++ driver/controller.go
@@ -0,0 +1,81 @@
+package driver
+
+import (
+	"github.com/HFO4/gbc-in-cloud/util"
+	"time"
+)
+
+type ControllerDriver interface {
+	// Initialize controller status
+	InitStatus(*byte)
+	// Update input status for emulator, called by the emulator
+	UpdateInput() bool
+	// Update input status for the controller itself
+	NewInput([]byte)
+}
+
+type TelnetController struct {
+	inputStatus *byte
+	Keymap      [8]KeyMap
+}
+
+type KeyMap struct {
+	// Timestamp when the key was last pressed
+	LastPress int64
+	// Timestamp when the emulator last check the input status
+	LastChecked int64
+}
+
+func (tel *TelnetController) InitStatus(statusPointer *byte) {
+	tel.inputStatus = statusPointer
+}
+
+func (tel *TelnetController) UpdateInput() bool {
+	var requestInterrupt bool
+	var statusCopy byte
+	statusCopy = *tel.inputStatus
+	timeNow := time.Now().UnixNano() / int64(time.Millisecond)
+	for key, offset := range tel.Keymap {
+		/*
+			If the key was pressed in 200ms,
+			We consider player "hold" the key.
+		*/
+		if timeNow-offset.LastPress <= 200 {
+			statusCopy = util.ClearBit(statusCopy, uint(key))
+			if timeNow-offset.LastChecked >= 100 {
+				requestInterrupt = true
+			}
+			offset.LastChecked = timeNow
+		} else {
+			statusCopy = util.SetBit(statusCopy, uint(key))
+		}
+	}
+	*tel.inputStatus = statusCopy
+	return requestInterrupt
+}
+
+func (tel *TelnetController) NewInput(data []byte) {
+
+	/*
+		Key map for the input key byte and the control
+		bit in Gameboy register.
+	*/
+	keyDataMap := map[byte]int{
+		65:  2,
+		66:  3,
+		68:  1,
+		67:  0,
+		10:  7,
+		8:   6,
+		122: 5,
+		120: 4,
+		0:   7,
+	}
+
+	key := data[len(data)-1]
+	keyId := keyDataMap[key]
+
+	timeNow := time.Now().UnixNano() / int64(time.Millisecond)
+	tel.Keymap[keyId].LastPress = timeNow
+
+}
---
+++ driver/gui.go
@@ -0,0 +1,120 @@
+package driver
+
+import (
+	"github.com/HFO4/gbc-in-cloud/util"
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+	"image/color"
+	"log"
+	"os"
+)
+
+type LCD struct {
+	pixels *[160][144][3]uint8
+	window *pixelgl.Window
+
+	pixelMap *pixel.PictureData
+
+	inputStatus *byte
+	title       string
+}
+
+func (lcd *LCD) Init(pixels *[160][144][3]uint8, title string) {
+	lcd.pixels = pixels
+	lcd.title = title
+	log.Println("[Display] Initialize GUI display")
+	lcd.pixelMap = pixel.MakePictureData(pixel.R(0, 0, 160, 144))
+
+}
+
+func (lcd *LCD) InitStatus(statusPointer *byte) {
+	lcd.inputStatus = statusPointer
+}
+
+func (lcd *LCD) UpdateInput() bool {
+	// Mapping from keys to GB index.
+	// Reference :https://github.com/Humpheh/goboy/blob/master/pkg/gbio/iopixel/pixels.go
+	var keyMap = map[pixelgl.Button]byte{
+		// A button
+		pixelgl.KeyZ: 5,
+		// B button
+		pixelgl.KeyX: 4,
+		// SELECT button
+		pixelgl.KeyBackspace: 6,
+		// START button
+		pixelgl.KeyEnter: 7,
+		// RIGHT button
+		pixelgl.KeyRight: 0,
+		// LEFT button
+		pixelgl.KeyLeft: 1,
+		// UP button
+		pixelgl.KeyUp: 2,
+		// DOWN button
+		pixelgl.KeyDown: 3,
+	}
+	var requestInterrupt bool
+	var statusCopy byte
+	statusCopy = *lcd.inputStatus
+	for key, offset := range keyMap {
+		if lcd.window.JustPressed(key) {
+			statusCopy = util.ClearBit(statusCopy, uint(offset))
+			requestInterrupt = true
+		}
+		if lcd.window.JustReleased(key) {
+			statusCopy = util.SetBit(statusCopy, uint(offset))
+			requestInterrupt = false
+		}
+	}
+
+	*lcd.inputStatus = statusCopy
+	return requestInterrupt
+}
+
+func (lcd *LCD) NewInput(b []byte) {
+
+}
+
+func (lcd *LCD) Run(drawSignal chan bool, onQuit func()) {
+	pixelgl.Run(func() {
+		lcd.run(drawSignal, onQuit)
+	})
+}
+
+func (lcd *LCD) run(drawSignal chan bool, onQuit func()) {
+	cfg := pixelgl.WindowConfig{
+		Title:  lcd.title,
+		Bounds: pixel.R(0, 0, 160*3, 144*3),
+		VSync:  false,
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+	lcd.window = win
+
+	go func() {
+		for !win.Closed() {
+		}
+		onQuit()
+		os.Exit(0)
+	}()
+
+	for {
+		// drawSignal was sent by the emulator
+		<-drawSignal
+		for y := 0; y < 144; y++ {
+			for x := 0; x < 160; x++ {
+				colour := color.RGBA{R: lcd.pixels[x][y][0], G: lcd.pixels[x][y][1], B: lcd.pixels[x][y][2], A: 0xFF}
+				lcd.pixelMap.Pix[(143-y)*160+x] = colour
+			}
+		}
+
+		graph := pixel.NewSprite(pixel.Picture(lcd.pixelMap), pixel.R(0, 0, 160, 144))
+		mat := pixel.IM
+		mat = mat.Moved(win.Bounds().Center())
+		mat = mat.ScaledXY(win.Bounds().Center(), pixel.V(3, 3))
+		graph.Draw(lcd.window, mat)
+		win.Update()
+	}
+
+}
